7ed604fd8d59192d0f642e29b4668afb0b017936
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-06 22:31:39 -0500

    set up unit test for camel case function

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color, and updates when clicked', () => {
 	render(<App />);
@@ -59,4 +59,18 @@ test('Clicked disabled button has gray background and reverts to blue', () => {
 
 	fireEvent.click(checkbox);
 	expect(button).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+describe('spaces before camel-case capital letters', () => {
+	test('Works for no inner capital letters', () => {
+		expect(replaceCamelWithSpaces('Red')).toBe('Red');
+	});
+
+	test('Works for one inner capital letter', () => {
+		expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+	});
+
+	test('Works for multiple inner capital letters', () => {
+		expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+	});
 });
