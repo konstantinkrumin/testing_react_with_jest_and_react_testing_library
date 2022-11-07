50b130945dd30a11534202a4d4e6320b173e447e
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-06 22:12:50 -0500

    set up checkbox for disabling button and corresponding tests

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 
 function App() {
 	const [buttonColor, setButtonColor] = useState('red');
+	const [disabled, setDisabled] = useState(false);
 
 	const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
 	return (
 		<div>
 			<button
+				disabled={disabled}
 				style={{ backgroundColor: buttonColor }}
 				onClick={() => setButtonColor(newButtonColor)}
 			>
 				Change to {newButtonColor}
 			</button>
+
+			<input
+				type="checkbox"
+				defaultChecked={disabled}
+				id="disable-button-checkbox"
+				onChange={e => setDisabled(e.target.checked)}
+			/>
+
+			<label htmlFor="disable-button-checkbox">Disable button</label>
 		</div>
 	);
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -12,3 +12,25 @@ test('button has correct initial color, and updates when clicked', () => {
 	expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
 	expect(colorButton).toHaveTextContent('Change to red');
 });
+
+test('initial conditions', () => {
+	render(<App />);
+
+	const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+	expect(colorButton).toBeEnabled();
+
+	const checkbox = screen.getByRole('checkbox');
+	expect(checkbox).not.toBeChecked();
+});
+
+test('Checkbox disables button on first click and enables on second click', () => {
+	render(<App />);
+	const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+	const button = screen.getByRole('button');
+
+	fireEvent.click(checkbox);
+	expect(button).toBeDisabled();
+
+	fireEvent.click(checkbox);
+	expect(button).toBeEnabled();
+});
