16ff251a3f76a3f1dc217a0811b84373fe631da0
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-10 00:51:49 -0500

    added popover and tests

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import SummaryForm from '../SummaryForm';
 
@@ -11,15 +12,33 @@ test('Initial conditions', () => {
 	expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox disables button on first click and enables on second', () => {
+test('Checkbox disables button on first click and enables on second', async () => {
+	const user = userEvent.setup();
+
 	render(<SummaryForm />);
 
 	const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
 	const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
 
-	fireEvent.click(checkbox);
+	await user.click(checkbox);
 	expect(confirmButton).toBeEnabled();
 
-	fireEvent.click(checkbox);
+	await user.click(checkbox);
 	expect(confirmButton).toBeDisabled();
+});
+
+test('Popover responds to hover', async () => {
+	const user = userEvent.setup();
+	render(<SummaryForm />);
+
+	const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+	expect(nullPopover).not.toBeInTheDocument();
+
+	const termsAndConditions = screen.getByText(/terms and conditions/i);
+	await user.hover(termsAndConditions);
+	const popover = screen.getByText(/no ice cream will actually be delivered/i);
+	expect(popover).toBeInTheDocument();
+
+	await user.unhover(termsAndConditions);
+	expect(popover).not.toBeInTheDocument();
 });
