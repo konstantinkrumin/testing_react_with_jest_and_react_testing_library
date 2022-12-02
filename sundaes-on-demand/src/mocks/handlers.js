d004110958389b1d60c9bdd47ca1aa9ce5f15f7e
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-12-02 00:33:44 -0500

    set up async test

--- sundaes-on-demand/src/mocks/handlers.js
+++ sundaes-on-demand/src/mocks/handlers.js
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 
 export const handlers = [
 	rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-		return rest(
+		return res(
 			ctx.json([
 				{ name: 'Chocolate', imagePath: '/images/chocolate.png' },
 				{ name: 'Vanilla', imagePath: '/images/vanilla.png' }
