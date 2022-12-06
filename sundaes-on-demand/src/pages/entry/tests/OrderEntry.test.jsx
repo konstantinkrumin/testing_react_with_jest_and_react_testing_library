9869a529e8f3a0f0ad3572f48ae7069d1f368812
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-12-06 00:53:12 -0500

    adding test for error cases

---
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+
+import { server } from '../../../mocks/server';
+
+import OrderEntry from '../OrderEntry';
+
+test('handles error for scoops and toppings routes', async () => {
+	server.resetHandlers(
+		rest.get('http://localhost:3030/scoops', (req, res, ctx) => ctx.status(500)),
+		rest.get('http://localhost:3030/toppings', (req, res, ctx) => ctx.status(500))
+	);
+
+	render(<OrderEntry />);
+
+	const alerts = await screen.findAllByRole('alert', {
+		name: 'An unexpected error occurred. Please try again later.'
+	});
+
+	expect(alerts).toHaveLength(2);
+});
