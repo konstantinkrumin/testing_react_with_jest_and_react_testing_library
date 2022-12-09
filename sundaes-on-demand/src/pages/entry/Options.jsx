d287dad4af876821e70508b36e0b7bbc73dd99ea
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-12-09 01:10:35 -0500

    set up UI part for the forms

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -2,6 +2,10 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 
+import { pricePerItem } from '../../constants';
+import { formatCurrency } from '../../utilities';
+import { useOrderDetails } from '../../contexts/OrderDetails';
+
 import AlertBanner from '../common/AlertBanner';
 
 import ScoopOption from './ScoopOption';
@@ -10,6 +14,7 @@ import ToppingOption from './ToppingOption';
 export default function Options({ optionType }) {
 	const [items, setItems] = useState([]);
 	const [error, setError] = useState(false);
+	const { totals } = useOrderDetails();
 
 	// optionType is 'scoops' or 'toppings'
 	useEffect(() => {
@@ -24,10 +29,20 @@ export default function Options({ optionType }) {
 	}
 
 	const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+	const title = optionType[0].toUpperCase() + optionType.slice(1);
 
 	const optionItems = items.map(item => (
 		<ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
 	));
 
-	return <Row>{optionItems}</Row>;
+	return (
+		<>
+			<h2>{title}</h2>
+			<p>{formatCurrency(pricePerItem[optionType])} each</p>
+			<p>
+				{title} total: {formatCurrency(totals[optionType])}
+			</p>
+			<Row>{optionItems}</Row>
+		</>
+	);
 }
--- sundaes-on-demand/src/pages/summary/OrderSummary.jsx
+++ sundaes-on-demand/src/pages/summary/OrderSummary.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import { useOrderDetails } from '../../contexts/OrderDetails';
+import { formatCurrency } from '../../utilities';
+
+import SummaryForm from './SummaryForm';
+
 const OrderSummary = () => {
-	return <div>OrderSummary</div>;
+	const { totals, optionCounts } = useOrderDetails();
+
+	const scoopArray = Object.entries(optionCounts.scoops);
+	const scoopList = scoopArray.map(([key, value]) => (
+		<li key={key}>
+			{value} {key}
+		</li>
+	));
+
+	const toppingsArray = Object.keys(optionCounts.toppings);
+	const toppingsList = toppingsArray.map(key => <li key={key}>{key}</li>);
+
+	return (
+		<div>
+			<h1>Order Summary</h1>
+			<h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+			<ul>{scoopList}</ul>
+			<h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+			<ul>{toppingsList}</ul>
+			<SummaryForm />
+		</div>
+	);
 };
 
 export default OrderSummary;
