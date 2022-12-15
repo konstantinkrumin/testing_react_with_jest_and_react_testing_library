95d63121a700d60b41e91accd633e953b8dfaaea
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-12-15 00:53:46 -0500

    added test condition for loading

--- sundaes-on-demand/src/tests/orderPhase.test.jsx
+++ sundaes-on-demand/src/tests/orderPhase.test.jsx
@@ -49,10 +49,16 @@ test('order phases for happy path', async () => {
 	});
 	await user.click(confirmOrderButton);
 
+	const loading = screen.getByText(/loading/i);
+	expect(loading).toBeInTheDocument();
+
 	const thankYouHeader = await screen.findByRole('heading', {
 		name: /thank you/i
 	});
 	expect(thankYouHeader).toBeInTheDocument();
+
+	const notLoading = screen.queryByText('loading');
+	expect(notLoading).not.toBeInTheDocument();
 
 	const orderNumber = await screen.findByText(/order number/i);
 	expect(orderNumber).toBeInTheDocument();
