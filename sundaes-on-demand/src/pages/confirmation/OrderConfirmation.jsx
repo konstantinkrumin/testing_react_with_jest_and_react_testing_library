3889318553c465b87ff22807f38d93cc03049ae8
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-12-15 01:36:09 -0500

    set up order confirmation test

--- sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
+++ sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+
+import AlertBanner from '../common/AlertBanner';
 import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function OrderConfirmation({ setOrderPhase }) {
 	const { resetOrder } = useOrderDetails();
 	const [orderNumber, setOrderNumber] = useState(null);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		axios
@@ -13,8 +16,19 @@ export default function OrderConfirmation({ setOrderPhase }) {
 			.then(response => {
 				setOrderNumber(response.data.orderNumber);
 			})
-			.catch(error => {});
+			.catch(error => setError(true));
 	}, []);
+
+	const newOrderButton = <Button onClick={handleClick}>Create new order</Button>;
+
+	if (error) {
+		return (
+			<>
+				<AlertBanner message={null} variant={null} />
+				{newOrderButton}
+			</>
+		);
+	}
 
 	function handleClick() {
 		resetOrder();
@@ -30,7 +44,7 @@ export default function OrderConfirmation({ setOrderPhase }) {
 				<p style={{ fontSize: '25%' }}>
 					as per our terms and conditions, nothing will happen now
 				</p>
-				<Button onClick={handleClick}>Create new order</Button>
+				{newOrderButton}
 			</div>
 		);
 	} else {
